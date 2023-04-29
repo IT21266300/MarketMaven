5070a6a97b5c49e71811fc57e34df93739ed0e42
IT21266300 <it21266300@my.sliit.lk>
2023-04-29 22:02:00 +0530

    connect firebase

--- app/src/main/java/com/example/marketmaven/MainActivity.kt
+++ app/src/main/java/com/example/marketmaven/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.marketmaven
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.seller_iterm_select)
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
     }
 }
