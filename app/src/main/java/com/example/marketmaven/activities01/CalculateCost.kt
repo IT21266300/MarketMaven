e699eab284c5100d865a3d7e376d591404198f05
Kiriwaththuduwa KCN <99331642+it21298158@users.noreply.github.com>
2023-05-06 14:55:09 +0530

    upload

---
+++ app/src/main/java/com/example/marketmaven/activities01/CalculateCost.kt
@@ -0,0 +1,51 @@
+package com.example.madproject1.activities01
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import com.example.madproject1.R
+
+class CalculateCost : AppCompatActivity() {
+
+    private lateinit var btnCalAdd: Button
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculate_cost)
+
+        btnCalAdd = findViewById(R.id.btnCalInsert)
+
+        btnCalAdd.setOnClickListener {
+            val intent = Intent(this, ItemDataInsert::class.java)
+            startActivity(intent)
+        }
+
+        val calculateButton = findViewById<Button>(R.id.button_calculate)
+        val weightEditText = findViewById<EditText>(R.id.sel_item_weight_edit)
+        val distanceEditText = findViewById<EditText>(R.id.sel_item_distance_edit)
+        val sellingPriceEditText = findViewById<EditText>(R.id.sel_item_distance_editt)
+        val itemSpinner = findViewById<Spinner>(R.id.item_spinner)
+
+        calculateButton.setOnClickListener {
+            val weight = weightEditText.text.toString().toDoubleOrNull()
+            val distance = distanceEditText.text.toString().toDoubleOrNull()
+            val selectedItem = itemSpinner.selectedItem.toString()
+
+            if (weight == null || distance == null) {
+                Toast.makeText(this, "Please enter valid weight and distance", Toast.LENGTH_SHORT).show()
+            } else {
+
+                val markup = if (selectedItem == "Vegetable") 1.1 else 1.3
+                val sellingPrice = weight * distance * markup
+//                val sellingPrice = (weight * distance * 1.1).toString()
+                sellingPriceEditText.setText(String.format("%.2f", sellingPrice))
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/marketmaven/activities01/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.madproject1.activities01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.madproject1.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnInsertData: Button
+    private lateinit var btnFetchData: Button
+    private lateinit var btnCalculateAmount: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_layout)
+
+        btnInsertData = findViewById(R.id.buttonMain1)
+        btnFetchData = findViewById(R.id.buttonMain2)
+        btnCalculateAmount = findViewById(R.id.buttonMain3)
+
+        btnInsertData.setOnClickListener {
+
+            val intent = Intent(this, ItemDataInsert::class.java)
+            startActivity(intent)
+        }
+        btnFetchData.setOnClickListener {
+            val intent = Intent(this, DisplayActivity::class.java)
+            startActivity(intent)
+        }
+        btnCalculateAmount.setOnClickListener {
+            val intent = Intent(this, CalculateCost::class.java)
+            startActivity(intent)
+        }
+
+//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_selling_item)
+//        setContentView(R.layout.activity_itemcost_history)
+//        setContentView(R.layout.activity_calculate_cost)
+//        setContentView(R.layout.activity_item_data_insert)
+
+
+
+
+
+
+    }
+}
